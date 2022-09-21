52d9851bbd9831237bc18d3e1ee55d9c7fa53cde
mehmet dedeler <mehmet.dedeler@ug.bilkent.edu.tr>
2022-09-21 15:56:06 +0300

    Add display menu and some bugfix
    This commit includes display menu and general bugfixes throughout the methods.

--- Application.java
+++ Application.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,21 +7,69 @@ public class Application {
     
     public static void main(String[] args) {
         int arraySize;
+        int input;
         int[] randArr; 
         Random r = new Random();
 
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Write the size of array: ");
-        arraySize = in.nextInt();
+        do {
+            System.out.print("Write the size of array: ");
+            arraySize = in.nextInt();
+        } while (arraySize <= 0);
 
         randArr = new int[arraySize];
         for (int i = 0; i < arraySize; i++) {
             randArr[i] = r.nextInt(101);
         }
+
+        boolean exitMenu = false;
+        while (!exitMenu) {
+            do {
+                System.out.println();
+                System.out.println("Enter choice");
+                System.out.println("1- find the maximum ");
+                System.out.println("2- find the minimum ");
+                System.out.println("3- find the avarage");
+                System.out.println("4- Find the sum ");
+                System.out.println("5-exit");
+                System.out.println();
+                input = in.nextInt();
+    
+    
+            }while( input < 1 || input > 5 );
+
+            if (input == 5) {
+                exitMenu = true;
+            }
+            else if (input == 1){
+                System.out.println("Your array: " + Arrays.toString(randArr));
+                System.out.print("maximum: ");
+                System.out.println(findMax(randArr));
+
+            }
+            else if (input == 2){
+                System.out.println("Your array: " + Arrays.toString(randArr));
+                System.out.print("minimum: ");
+                System.out.println(findMin(randArr));
+
+            }
+            else if (input == 3){
+                System.out.println("Your array: " + Arrays.toString(randArr));
+                System.out.println("The average is: " + findAverage(randArr) + " and differences are " + Arrays.toString(differenceFromAverage(randArr)));
+
+            }
+            else if  (input == 4){
+                System.out.println("Your array: " + Arrays.toString(randArr));
+                System.out.println("The summation of odd indexes: " + findTheSumOfOddIndexes(randArr));
+                System.out.println("The summation of even indexes: " + findTheSumOfEvenIndexes(randArr));
+            }
+        }
+
+        in.close();
         
     }
-    public int findMin(int[] randArr){
+    public static int findMin(int[] randArr){
         int min = Integer.MAX_VALUE;
         for(int i= 0; i<randArr.length;i++){
             if(randArr[i]<min)
@@ -30,7 +79,7 @@ public class Application {
         }
         return min;
     }
-    public int findMax(int[] randArr){
+    public static int findMax(int[] randArr){
         int max = Integer.MIN_VALUE;
         for(int i= 0; i<randArr.length;i++){
             if(randArr[i]>max)
@@ -58,7 +107,7 @@ public class Application {
     }
 
 
-    public int findAverage(int[] intArray) {
+    public static int findAverage(int[] intArray) {
         
         int average;
         int sum = 0;
@@ -72,7 +121,7 @@ public class Application {
         return average;
     }
 
-    public int[] differenceFromAverage(int[] intArray) {
+    public static int[] differenceFromAverage(int[] intArray) {
         int[] differenceFromAverage = new int[intArray.length];
         
         int average = findAverage(intArray);
